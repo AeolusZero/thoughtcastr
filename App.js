2ab7613402b1a35001e358b98a7e527a6e4ef139
AeolusZero <42942337+AeolusZero@users.noreply.github.com>
2018-09-08 20:29:51 -0400

    backup of App before pasting new code

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from 'react';  //to use React objects, you have to do this import operation.  Basically any file that uses React starts with this command.
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '@material-ui/core/Button';
 
-export default class App extends React.Component {
+export default class App extends React.Component {  //
   render() {
     return (
       <View style={styles.container}>
@@ -9,6 +10,8 @@ export default class App extends React.Component {
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Text>Hello yes hello</Text>
+        <Greeting name="Harrison" lastName="Holland"></Greeting>
+        <Button variant="contained" color="primary">Press Me</Button>
       </View>
     );
   }
@@ -22,3 +25,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+class Greeting extends React.Component { // we got this from the website facebook.github.io/react-native/docs/props
+  render() {
+    return ( // whatever is inside the Return method will be treated like HTML
+      <Text>Hello {this.props.name}!</Text>  //Whatever is inside the curly brackets will be treated like JavaScript
+    );
+  }
+}
+
+// A state is just a sort of configuration of an object.
---
+++ Module.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Moduloso extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}><Text>Wiggity waggaty</Text></View>
+            
+        );
+      }
+}
+export default Moduloso
